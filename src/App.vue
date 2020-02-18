1e18b09051eb25a7682c1175178cf4a0b94cb636
elle <liubou.grebennikova@gmail.com>
2020-02-18 14:24:43 +0300

    Токен записывается в store

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,13 @@
 <template>
   <div id="app">
+    <span id="success"></span>
+    <div>Total: {{token}}</div>
+    <button @click="setToken">{{token}}</button>
     <section v-if="errored">
       <p>The information is not available at the moment, please try back later</p>
     </section>
     <section v-else>
-      <iframe v-if="starting" class="wialon__form" v-bind:src="getUrl"></iframe>
+      <iframe v-if="starting" class="wialon__form" v-bind:src="url"></iframe>
       <div v-else-if="loading">Loading...</div>
       <div v-else>
         <Header></Header>
@@ -24,10 +27,7 @@
 
 
 import Header from './components/Header.vue'
-//import axios from "axios";
-//import VueAxios from "vue-axios";
-
-
+import { mapState, mapMutations, mapGetters, mapActions } from 'vuex';
 
 export default {
   components: {
@@ -42,20 +42,54 @@ export default {
     };
   },
   computed: {
-     getUrl() {
-      return this.$store.state.url
-     }
+    ...mapGetters([
+      'url',
+      'token',
+      'total',
+      'authenticated',
+    ])  
   },
-
+  methods: {
+    ...mapMutations([
+      'plusToken',
+      'isAuth'
+      ]),
+    setToken() {
+      
+      this.$store.commit('setToken', token)
+    },
+    ...mapActions([
+      'loadToken'
+      ])
+  },
+     
   filters: {
     
   },
   mounted() {
     
+  },
+  created() {
+    this.$store.dispatch('loadToken');
   }
 }
 
-
+/*let token;
+window.onmessage = function (e) {
+  var msg = e.data;
+  if (typeof msg == "string" && msg.indexOf("access_token=") >= 0) {
+    token = msg.replace("access_token=", "");
+    console.log('первый вывод', token)
+  var el = document.getElementById("success");
+    if (el) {
+      el.innerHTML = "Your token: " + token;
+    }
+  }
+};
+      
+store.dispatch
+*/
+   
 </script>
 
 <style lang="less">
--- src/store/index.js
+++ src/store/index.js
@@ -1,47 +1,59 @@
-import Vue from "vue";
-import Vuex from "vuex";
+import Vue from 'vue';
+import Vuex from 'vuex';
+//import App from '../App.vue'
 
 Vue.use(Vuex);
 
+const URL = "https://hosting.wialon.com/login.html?client_id=myApp&access_type=0x100&activation_time=0&duration=604800&flags=0x1&redirect_uri=https://hosting.wialon.com/post_token.html"
 
 export default new Vuex.Store({
 
   state: {
-  	count: 0,
-  	token: null,
-      user: {
-        name: null
-      },
+    count: 5,
+    token: 0,
+    user: {
+      name: null
+    },
+    authenticated: null,  
     currentMessage: null,
     objects: [],
     feature: [],
-    url: "https://hosting.wialon.com/login.html?",
+    url: URL
   },
   getters: {
-
+    url: state => {
+      return state.url
+    },
+    token: state => {
+      return state.token;
+    },
+    total: state => {
+      return state.count * state.token;
+    },
+    authenticated: state => {
+      return state.authenticated;
+    }
   },
   mutations: {
-  
-    openWin: state => {
-      window.open(state.url, "_blank", "width=760, height=500, top=300, left=500");
+    SET_TOKEN(state, payload) {
+      state.token = payload;
     },
-    // Get token message, which is sent from auth window to parent window
-    // when redirect_uri is set to hosting.wialon.com/post_token.html
-    getToken(state) {
-      window.onmessage = function (e) {
-        let msg = e.data;
-        if (typeof msg == "string" && msg.indexOf("access_token=") >= 0) {
-          state.token = msg.replace("access_token=", "");
-        return state.token;
-        }
-      }
-    },
-
+    IS_AUTH(state){
+      state.authenticated = 'done';
+    }
   },
 
   actions: {
-  	
-
+    loadToken(context) {
+      let token;
+      window.onmessage = function (e) {
+        let msg = e.data;
+        if (typeof msg == "string" && msg.indexOf("access_token=") >= 0) {
+          token = msg.replace("access_token=", "");
+          context.commit('SET_TOKEN', token)
+        }
+      }
+    }      
   },
 
   modules: {}
